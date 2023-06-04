0adcaa602cdf906ab2aece2dcd3c48ee1fdc6b44
David Weaver <david.w@playsidestudios.com>
2023-06-05 09:29:03 +1000

    Added movement commands from an abstract command

--- MinigameChallenge_BumperBalls/Assets/Scripts/MoveCommand.cs
+++ MinigameChallenge_BumperBalls/Assets/Scripts/MoveCommand.cs
@@ -2,55 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveCommand : MonoBehaviour
+public abstract class Command
 {
-    public class MoveForward : ICommand
+    public abstract void Execute();
+}
+
+public class MoveUpCommand : Command
+{
+    private PlayerMovement _player;
+
+    public MoveUpCommand(PlayerMovement player)
     {
-        public void Execute()
-        {
-            //instantiate the player movement class 
-            //Player.PlayerMovement pm = new Player.PlayerMovement();
-            //Call the function off that class
-            //pm.ForwardMove();
-            Debug.Log("Testing Forward");
-        }
+        _player = player;
     }
 
-    public class MoveBack : ICommand
+    public override void Execute()
     {
-        public void Execute()
-        {
-            //Player.PlayerMovement pm = new Player.PlayerMovement();
-            //pm.BackMove();
-            Debug.Log("Testing Back");
-        }
+        _player.MoveUp();
+    }
+}
+
+public class MoveDownCommand : Command
+{
+    private PlayerMovement _player;
+
+    public MoveDownCommand(PlayerMovement player)
+    {
+        _player = player;
     }
 
-    public class MoveLeft : ICommand
+    public override void Execute()
     {
-        public void Execute()
-        {
-            //Player.PlayerMovement pm = new Player.PlayerMovement();
-            //pm.LeftMove();
-            Debug.Log("Testing Left");
-        }
-    }
-
-    public class MoveRight : ICommand
-    {
-        public void Execute()
-        {
-            //Player.PlayerMovement pm = new Player.PlayerMovement();
-            //pm.RightMove();
-            Debug.Log("Testing Right");
-        }
-    }
-
-    public class DoNothing : ICommand
-    {
-        public void Execute()
-        {
-            Debug.Log("Testing Nothing");
-        }
+        _player.MoveDown();
     }
 }
